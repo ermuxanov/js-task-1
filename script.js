42a8b8fd72d669e666c68b497c7458df7878432f
ermuxanov <74331498+ermuxanov@users.noreply.github.com>
2022-10-06 15:47:36 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ var age = prompt("Введите свой возраст");
 var myInfo = "Я " + surName + " " + firstName + " " + "мне " + age;
 console.log(myInfo);
 alert(myInfo);
+
 var sumAnswer = prompt("Введите свой ответ: 12 + 34 = ?");
 var sum = 12 + 34;
-console.log("12 + 34" + " = " + sum + " (Ваш ответ " + sumAnswer + ")");
+alert("12 + 34" + " = " + sum + " (Ваш ответ " + sumAnswer + ")");
 var minusAnswer = prompt("Введите свой ответ: 50 - 19 = ?");
 var minus = 50 - 19;
-console.log("50 - 19" + " = " + minus + " (Ваш ответ " + minusAnswer + ")");
+alert("50 - 19" + " = " + minus + " (Ваш ответ " + minusAnswer + ")");
 var multiplyAnswer = prompt("Введите свой ответ: 9 * 7 = ?");
 var multiply = 9 * 7;
-console.log("9 * 7" + " = " + multiply + " (Ваш ответ " + multiplyAnswer + ")");
+alert("9 * 7" + " = " + multiply + " (Ваш ответ " + multiplyAnswer + ")");
 var divisionAnswer = prompt("Введите свой ответ: 57 % 9 = ?");
 var division = 57 % 9;
-console.log(
-  "57 % 9" + " = " + division + " (Ваш ответ " + divisionAnswer + ")"
-);
+alert("57 % 9" + " = " + division + " (Ваш ответ " + divisionAnswer + ")");
+console.log("12 + 34" + " = " + sum + " (Ваш ответ " + sumAnswer + ")");
+console.log("50 - 19" + " = " + minus + " (Ваш ответ " + minusAnswer + ")");
+console.log("9 * 7" + " = " + multiply + " (Ваш ответ " + multiplyAnswer + ")");
+console.log("57 % 9" + " = " + division + " (Ваш ответ " + divisionAnswer + ")");
